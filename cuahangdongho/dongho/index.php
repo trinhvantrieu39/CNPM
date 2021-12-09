122de95412dbdf73b09405f68578fe260614541c
trinhvantrieu39 <75432927+trinhvantrieu39@users.noreply.github.com>
2021-12-09 21:58:47 +0700

    header

---
+++ cuahangdongho/dongho/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	
+	<title>HILL TOP WATCH</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.css">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+	<script src="jquery/jquery.js"></script>
+	<script src="bootstrap/js/bootstrap.js"></script>
+	<link rel="stylesheet" href="css/style.css">
+
+
+
+	
+</head>
+<body>
+
+	<div class="container" style="width: 100%;"> 
+		<div class="header col-xs-12 col-sm-12 col-md-12 col-lg-12" style="background-image: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,88,121,1) 35%, rgba(0,212,255,1) 100%);">
+				<?php include('header.php'); ?>		
+		</div>
+		
+	</div>
+
+
+</body>
+</html>
+
