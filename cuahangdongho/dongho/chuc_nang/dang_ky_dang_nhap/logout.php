1cb1634f373333c1aedf06d5998055919187e0d5
levietsanh <levietsanh123@gmail.com>
2021-12-19 23:55:25 +0700

    delete

--- cuahangdongho/dongho/chuc_nang/dang_ky_dang_nhap/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-	unset($_SESSION['login']);
-	echo "<script> location.href='index.php'; </script>";
-?>
--- cuahangdongho/dongho/right.php
+++
@@ -1,3 +0,0 @@
-<?php  
-	include('chuc_nang/dang_ky_dang_nhap/dang_ky_dang_nhap.php');
-?>
